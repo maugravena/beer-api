6439ec3e3f8760b4f417558684f099a49ebfebfd
Mauricio <maugravena@gmail.com>
2019-12-07 18:27:23 -0300

    add and calculate average

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,8 @@
 class Beer < ApplicationRecord
   validates :style, :min_temperature, :max_temperature, presence: true
+  before_save :calculate_avg
+
+  def calculate_avg
+    self.average_temperature = (self.min_temperature + self.max_temperature).to_f / 2
+  end
 end
